0ce3e685b055bb301cd01b0162e024d775cff432
Nerotas <Nerotas7@gmail.com>
2025-09-03 17:24:08 -0500

    Create IntToRomain.ts

---
+++ IntToRomain.ts
@@ -0,0 +1,12 @@
+//medium: https://leetcode.com/problems/integer-to-roman/
+//Given an integer, convert it to a roman numeral.
+//Input is guaranteed to be within the range from 1 to 3999.
+function intToRoman(num: number): string {
+  return "I'm working on it!!!";
+}
+//test cases
+console.log("expected output: III", intToRoman(3)); // Output: "III"
+console.log("expected output: IV", intToRoman(4)); // Output: "IV"
+console.log("expected output: IX", intToRoman(9)); // Output: "IX"
+console.log("expected output: LVIII", intToRoman(58)); // Output: "LVIII"
+console.log("expected output: MCMXCIV", intToRoman(1994)); // Output: "MCMXCIV"
